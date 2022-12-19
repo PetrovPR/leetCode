ddee2a428e79b6ac32318aab44e85d0d8ce920c4
Pavel_Petrov <pavel_petrov2@epam.com>
2022-12-19 21:54:44 +0100

    3 on leetcode

--- src/main/java/org/leetcode/LongestSubstringWithoutRepeatingCharacters_3.java
+++ src/main/java/org/leetcode/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -17,7 +17,7 @@ public class LongestSubstringWithoutRepeatingCharacters_3 {
 
         for (int i = 0, j = 0; j < s.length() ; j ++) {
             if (map.containsKey(s.charAt(j))) {
-                i = map.get(s.charAt(j));
+                i = Math.max(map.get(s.charAt(j)), i);
                 max = Math.max(max, j - i + 1);
             }
             map.put(s.charAt(j), j + 1);
